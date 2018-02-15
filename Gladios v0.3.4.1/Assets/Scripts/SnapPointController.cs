d9338d918e7b79909ef937cb0c3c36504b21293a
bazzadwarf <bazzadobs@live.co.uk>
2018-02-15 17:01:14 +0000

    V0.3.4.1
    Stuff (Equiptment snapping and snapping points!)

---
+++ Gladios v0.3.4.1/Assets/Scripts/SnapPointController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapPointController : MonoBehaviour {
+
+    public GameObject parent;
+    public GameObject thisObject;
+
+	// Use this for initialization
+	void Start ()
+    {
+        parent.transform.SetParent(thisObject.transform);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
