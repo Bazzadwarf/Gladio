536b9e827114446857cce636a66c819374778dcd
Bazzadwarf <bazzadobs@live.co.uk>
2018-04-16 02:46:22 +0100

    Implemented game flow and expaned main menu.
    Need to add a weapon selection in menu and some highscore saving. Need to test in a meeting.

--- Gladios v0.9.1.2/Assets/Scripts/PlayerInfo.cs
+++ Gladios v0.9.1.2/Assets/Scripts/PlayerInfo.cs
@@ -2,25 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerInfo : MonoBehaviour {
 
+    public GameObject hudPanel;
     public float maxHealth;
     public Slider healthBar;
+    public Text killCounterText;
+    public Text timeAliveText; 
+
+    public GameObject scoreScreen;
+    public Text finalKillCount;
+    public Text finalTimer;
+    public Text finalScore;
 
     float currHealth;
     GameObject hitObject;
     bool inBox = false;
 
+    public int killCounter;
+    float timeAlive;
+
+    private string secs;
+    private string mins;
+
+    float wait = 10;
+
     // Use this for initialization
     void Start () {
         currHealth = maxHealth;
         RefreshHealthBar();
+        RefreshKillCounter();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(currHealth > 0)
+        {
+            RefreshKillCounter();
+            RefreshTimer();
+        }
 	}
     void OnTriggerEnter(Collider col)
     {
@@ -35,7 +57,13 @@ public class PlayerInfo : MonoBehaviour {
             if (currHealth <= 0)
             {
                 Debug.Log("Dead");
-                UnityEditor.EditorApplication.isPlaying = false;
+                this.DisplayScore();
+                while(wait > 0)
+                {
+                    wait -= Time.deltaTime;
+                }
+                SceneManager.LoadScene("MenuScene");
+                //UnityEditor.EditorApplication.isPlaying = false;
             }
         }
     }
@@ -51,5 +79,28 @@ public class PlayerInfo : MonoBehaviour {
     void RefreshHealthBar()
     {
         healthBar.value = (currHealth / maxHealth);
+    }
+
+    void RefreshKillCounter()
+    {
+        killCounterText.text = killCounter.ToString();
+    }
+
+    void RefreshTimer()
+    {
+        timeAlive += Time.deltaTime;
+        secs = Mathf.RoundToInt((timeAlive % 60)).ToString();
+        mins = Mathf.RoundToInt(((timeAlive % 60) % 60)).ToString();
+        timeAliveText.text = (mins + ":" + secs);
+    }
+
+    void DisplayScore()
+    {
+        
+        finalKillCount.text = killCounter.ToString();
+        finalTimer.text = (mins + ":" + secs);
+        finalScore.text = Mathf.RoundToInt(killCounter * timeAlive).ToString();
+        hudPanel.SetActive(false);
+        scoreScreen.SetActive(true);
     }
 }
---
+++ Gladios v0.9.1.2/Assets/Scripts/UI Scripts/UIQuitApplication.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIQuitApplication : MonoBehaviour {
+
+	public void CloseApplication(GameObject currentPanel) 
+	{
+		currentPanel.SetActive(false);
+		Application.Quit();
+	}
+}
